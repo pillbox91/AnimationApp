138ca11ea0335cc9727eaeea98e04d4c1d42ce40
Andrey Averyanov <averyanov1996@gmail.com>
2021-12-26 15:30:32 +0300

    0

--- AnimationApp/ViewController.swift
+++ AnimationApp/ViewController.swift
@@ -8,17 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet var coreAnimationView: UILabel!
+    
+    @IBOutlet var coreAnimationView: UIView!
+    @IBOutlet weak var testCoreAnimationView: UIView!
+    
+    private var originCoordinate: CGFloat?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    }
-
-    @IBAction func runCoreAnimationButton(_ sender: UIButton) {
-        sender.pulsate()
+//        originCoordinate = coreAnimationView.frame.origin.x
     }
     
+    @IBAction func runCoreAnimationButton(_ sender: UIButton) {
+        sender.pulsate()
+        
+        coreAnimationView.frame.origin.x -= 40
+        testCoreAnimationView.frame.origin.x -= 40
+    }
 }
 
