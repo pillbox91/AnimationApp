00d0dac55eaa2e50bae44c48ef942fdd70ef540e
Andrey Averyanov <averyanov1996@gmail.com>
2022-01-01 23:23:19 +0300

    Final

--- AnimationApp/ViewController.swift
+++ AnimationApp/ViewController.swift
@@ -5,19 +5,34 @@
 //  Created by Андрей Аверьянов on 24.12.2021.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
     
     @IBOutlet var coreAnimationView: UIView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
+    @IBOutlet var springAnimationView: SpringView!
     
     @IBAction func runCoreAnimationButton(_ sender: UIButton) {
         sender.pulsate()
+        
+        UIView.animate(
+            withDuration: 0.5,
+            delay: 0,
+            options: [.autoreverse, .repeat]) {
+                self.coreAnimationView.frame.origin.x -= 40
+            }
+        
+    }
+    
+    @IBAction func runSpringAnimationbutton(_ sender: SpringButton) {
+        springAnimationView.animation = "wobble"
+        springAnimationView.curve = Spring.AnimationCurve.EaseInCirc.rawValue
+        springAnimationView.force = 2
+        springAnimationView.duration = 1
+        springAnimationView.animate()
+        
+        sender.animation = "swing"
+        sender.animate()
     }
 }
 
